dc9053701887addc430e08ab04e156de4df7572b
Erfan Abdi <erfangplus@gmail.com>
2025-12-25 15:49:41 +0000

    RMX2020: Switch to shared libs to MediaTek framework jars
    * https://github.com/sarthakroy2002/packages_apps_ImsService/commit/462017dd0c43afe6f8c67ebff36223aec5a9d719
    
    Change-Id: Ib73f709b97ffcda9698ad4a9a4d5b6cd50ecab3b

--- RMX2020-ims.mk
+++ RMX2020-ims.mk
@@ -7,12 +7,3 @@ PRODUCT_PACKAGES += \
     mediatek-telecom-common \
     mediatek-telephony-base \
     mediatek-telephony-common \
-
-PRODUCT_BOOT_JARS += \
-    mediatek-common \
-    mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common
